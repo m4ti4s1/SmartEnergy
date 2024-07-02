0da567d804afedddeff13974c76f695161e7bc71
M4ti4s1 <m4ti4s.404@gmail.com>
2024-07-02 18:43:04 -0400

    Clase con constructor y metodos basicos para crear usuarios

---
+++ Usuario.java
@@ -0,0 +1,111 @@
+
+
+
+/* 
+Cosntructor
+    nombre
+    password
+    email
+    dispositivos
+    empresa electrica
+Metodos
+    agregar dispositivo
+    quitar dispositivo
+    ver lista dispositivos
+    obtener informacion usuario
+    consumo diario, mensual
+    
+*/
+import java.util.ArrayList;
+class Usuario {
+    private ArrayList<Dispositivo>  dispositivos;
+    private String nombre;
+    private String password;
+    private String email;
+    private String empresa;
+    
+    public Usuario(String nombre, String password, String email, String empresa) {
+        this.nombre = nombre;
+        this.password = password;
+        this.email = email;
+        this.empresa = empresa;
+        this.dispositivos = new ArrayList<>();
+    }
+    
+    public void cambiarPassword(String oldPassword, String newPassword) {
+
+        if (this.password.equals(oldPassword)) {
+            this.password = newPassword;
+        } else {
+            System.out.println("Old Password incorrect");
+        }
+    }
+    
+
+    /**
+     * Metodo para agregar un dispositivo a un usuario
+     * @param dispositivo dispositivo
+     */
+    public void addDispositivo(Dispositivo dispositivo) {
+        dispositivos.add(dispositivo);
+    }
+    
+    /**
+     * 
+     */
+    public void getInfo() {
+        System.out.println("Nombre: " + this.nombre);
+        System.out.println("email: " + this.email);
+        System.out.println("Empresa: " + this.empresa);
+        
+        ArrayList<String> dispositivosTipo = new ArrayList<>();
+        ArrayList<String> dispositivosMarca = new ArrayList<>();
+        ArrayList<Double> dispositivosConsumo = new ArrayList<>();
+
+        for (Dispositivo dispositivo : dispositivos) {
+            dispositivosTipo.add(dispositivo.getTipo());
+            dispositivosMarca.add(dispositivo.getMarca());
+            dispositivosConsumo.add(dispositivo.getConsumo());
+        }
+        
+
+        System.out.println("Lista Dispositivos"); 
+        System.out.printf("%-15s %-15s %-10s", "Tipo", "Marca", "Consumo");
+        for (int i = 0; i < dispositivosTipo.size(); i++) {
+            System.out.printf("%n%-15s %-15s %-.2f", dispositivosTipo.get(i), dispositivosMarca.get(i), dispositivosConsumo.get(i));
+        }
+
+    }
+
+    public void getConsumoMensual() {
+        String empresaCalculo = this.empresa;
+        double consumoTotalDispositivos = 0;
+        double iva, subtotal, cargoFijo, kWh, cargoTransporte, cargoCompra;
+        
+        double totalFinal = 0;
+        
+        for (Dispositivo dispositivo : dispositivos) {
+            consumoTotalDispositivos += dispositivo.getConsumo();
+        }
+
+
+            
+        empresaCalculo = empresaCalculo.toLowerCase();
+        switch (empresaCalculo) {
+            case "cge":
+                cargoFijo = 977.35;
+                kWh = 93.975;
+                cargoCompra = 21.519;
+                cargoTransporte = 3732;
+
+                subtotal = ((kWh + cargoCompra) * (consumoTotalDispositivos)) + cargoFijo + cargoTransporte;
+                iva = subtotal * 0.19;
+
+                totalFinal = subtotal + iva;
+
+                break;
+        }
+        
+        System.out.println("Consumo Mensual: " + totalFinal);
+    }
+}
