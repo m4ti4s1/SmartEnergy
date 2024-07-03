ff36ec1423356b5be0a18e633c4c7e62ed319622
M4ti4s1 <m4ti4s.404@gmail.com>
2024-07-03 18:35:35 -0400

    Haciendo pruebas de metodos para dispositivos y usuarios

--- Interfaz.java
+++ Interfaz.java
@@ -1,6 +1,18 @@
 // Clase para manejar la "aplicacion"
 public class Interfaz {
     public static void main(String[] args) {
+        Usuario user1 = new Usuario("matias", "12345", "matias@gmail.com", "cge");
+
+        Dispositivo notebook = new Dispositivo("Notebook", "Asus", "Rog Zephyrus", 51);
+        Dispositivo TV = new Dispositivo("TV", "LG", "Smart TV", 51);
+        Dispositivo Refrigerador = new Dispositivo("Electrodomestico", "Samsung", "NewOne", 52);
+
+        user1.addDispositivo(notebook);
+        user1.addDispositivo(Refrigerador);
+        user1.addDispositivo(TV);
         
+        user1.listaDispositivos();
+        user1.removeDispositivo(TV);
+        user1.listaDispositivos();
     }
 }
